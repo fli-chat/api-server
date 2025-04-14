367db6db72b89f967448436aba820d20943e9329
pie <yejinh_h@naver.com>
2025-04-14 16:06:14 +0900

    settings : 프로젝트 초기 설정

---
+++ filchat/src/main/java/com/project/filchat/FilchatApplication.java
@@ -0,0 +1,13 @@
+package com.project.filchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilchatApplication.class, args);
+    }
+
+}
---
+++ filchat/src/test/java/com/project/filchat/FilchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.filchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
